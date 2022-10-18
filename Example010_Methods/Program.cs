72f456f939eb52e6729d21b33c0809dafec7b0be
odrinskii <ananjev.arseniy@gmail.com>
2022-10-18 21:04:26 +0300

    Added Example 010

---
+++ Example010_Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 2, 34, 32, 43, 45 };
+
+int n = array.Length;
+int find = 2;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //остановка ветвления
+    }
+    //index = index +1;
+    index++;
+
+}
