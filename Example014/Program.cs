d343963ea40d2c217a495eba2dd20b37f9022855
odrinskii <ananjev.arseniy@gmail.com>
2022-11-13 23:26:44 +0300

    Added examples from lection_4

---
+++ Example014/Program.cs
@@ -0,0 +1,42 @@
+﻿// string[,] table = new string[2, 5];
+// // //По умолчаеию пустыми строками заполняется массив
+// // table[1, 2] = "Слово";
+
+// // for (int rows = 0; rows < table.GetLength(0); rows++)
+// // {
+// //     for (int columns = 0; columns < table.GetLength(1); columns++)
+// //     {
+// //         Console.Write($"-{table[rows, columns]}-");
+// //     }
+// // }
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr) // Метод генерации и зполнения нового массива случайными числами в неком диапазоне
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine("Новый сгенерированный массив: ");
+PrintArray(matrix);
---
+++ Example016 Recursion/Program.cs
@@ -0,0 +1,28 @@
+﻿// double Factorial(int n) // метод поиска факториала
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1); // использование рекурсии
+// }
+
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! - {Factorial(i)}");
+// }
+
+//Фибоначчи
+
+// f(1) = 1
+// f(2) = 2
+// f(n) = f(n-1) + f(n-2)
+
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
